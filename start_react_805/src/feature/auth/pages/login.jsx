9148d0ba0ace648861234c003ed2dc9568573529
yeison-bf <yeison.barrios.funieles@gmail.com>
2025-05-19 11:31:59 -0500

    ok

--- start_react_805/src/feature/auth/pages/login.jsx
+++ start_react_805/src/feature/auth/pages/login.jsx
@@ -2,39 +2,47 @@ import React, { useState } from "react";
 
 const Login = () => {
 
-    const [email, setEmail ] = useState("");
-    const [password, setPassword] = useState("");
-    const dataLocalStorage = JSON.parse(localStorage.getItem('access'))
-    function handdleSubmit(e){
-        e.preventDefault();
 
-        console.log(({email, password}))
+    const [username, SetUserName] = useState('Maria')
+    const [password, SetPassword] = useState('')
+    const [validateCredential, setValidateCredential] = useState(false)
+    const [dataStorage, setDataStorage] = useState(JSON.parse(localStorage.getItem ('access805')))
 
-        if(email == dataLocalStorage.email && password == dataLocalStorage.password){
-            alert("Si puede ingresar")
-        }else{
-            alert("Pailas....")
+
+    const handdleSubmit = (e) => {
+        e.preventDefault()
+        console.log(dataStorage)
+        if(username == '' || password == ''){
+            setValidateCredential(true)
+            return
         }
+        console.log({username, password})
 
+        if(username == dataStorage.username && password == dataStorage.password){
+            console.log("Registro exitoso")
+        }else{
+            SetPassword('')
+            SetUserName('')
+            console.log("Credenciales incorrectas")
+        }
     }
 
 
-
-    
-
     return (
         <>
-            <div className="container vh-100 w-100 d-flex justify-content-center aling-items-center">
+            <div className="container pt-5 w-100 d-flex justify-content-center aling-items-center">
                 <div className="card">
-                    <form onSubmit={ handdleSubmit }>
+                    <form onSubmit={ handdleSubmit } >
                         <img src="https://img.freepik.com/vector-premium/vector-libre-hermoso-elemento-diseno-colibri-volador-pancartas-carteles-folletos-folletos_1009653-1.jpg?semt=ais_hybrid&w=740" className="img-fluid w-50" alt="" />
                         <div className="mb-3">
                             <label className="form-label">Email</label>
-                            <input type="email" onChange={ (e) => setEmail(e.target.value) } className="form-control" placeholder="name@example.com" />
+                            <input value={username} onChange={ (e)=> SetUserName(e.target.value) } type="text" className="form-control"  />
+                            { validateCredential ? (<small className="text-danger">Este campo requerido</small>) : '' }
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Password</label>
-                            <input type="text" onChange={ (e) => setPassword(e.target.value)} className="form-control" placeholder="name@example.com" />
+                            <input value={password} type="text" onChange={ (e)=> SetPassword(e.target.value) }className="form-control"  />
+                            { validateCredential ? (<small className="text-danger">Este campo requerido</small>) : '' }
                         </div>
                         <div className="mb-3">
                             <input type="submit" value="Acceder" />
